1eadf53e846e688de0a74afef059c3c5e53c53e6
akadotsh <devakash256@gmail.com>
2026-08-19 23:52:46 +0530

    Add OCR image input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
-
+use leptess::LepTess;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-   pattern: String
+   path: String
 }
 fn main() {
-      let args = Cli::parse();
-      print!("args: {:?}",args,)
+      let cli = Cli::parse();
+      let mut lt =  LepTess::new(None,"eng").unwrap();
+      lt.set_image(&cli.path)
+        .unwrap();
+      println!("{}", lt.get_utf8_text().unwrap());
+
 }
