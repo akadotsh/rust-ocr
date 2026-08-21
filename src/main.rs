65cd2a868fa68afcd295584a47da6e65edf9906a
akadotsh <devakash256@gmail.com>
2026-08-21 13:29:25 +0530

    refactor

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,72 @@ fn extract_pdf_text(path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn extract_image_text(path: &Path) {
+    let mut lt = LepTess::new(None, "eng").unwrap();
+    lt.set_image(path).unwrap();
+    lt.set_variable(leptess::Variable::PreserveInterwordSpaces, "11")
+        .unwrap();
+    let tsv = lt.get_tsv_text(0).unwrap();
+
+    let mut words = Vec::new();
+
+    for line in tsv.lines().skip(1) {
+        let columns: Vec<&str> = line.split('\t').collect();
+
+        if columns.len() < 12 {
+            continue;
+        }
+
+        let conf: f32 = columns[10].parse().unwrap_or(0.0);
+
+        if conf < 70.0 {
+            continue;
+        }
+        let text = columns[11].to_string();
+
+        if text.trim().is_empty() {
+            continue;
+        }
+
+        words.push(Word {
+            text,
+            left: columns[6].parse().unwrap(),
+            top: columns[7].parse().unwrap(),
+            conf,
+        });
+    }
+
+    words.sort_by_key(|w| w.top);
+
+    let mut lines: Vec<Vec<&Word>> = Vec::new();
+
+    for word in &words {
+        if let Some(last_line) = lines.last_mut() {
+            let last_top = last_line[0].top;
+
+            if (word.top - last_top).abs() < 10 {
+                last_line.push(word);
+                continue;
+            }
+        }
+        lines.push(vec![word]);
+    }
+
+    for line in &mut lines {
+        line.sort_by_key(|w| w.left);
+    }
+
+    for line in lines {
+        let text = line
+            .iter()
+            .map(|w| w.text.as_str())
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        println!("{}", text);
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
@@ -41,69 +107,7 @@ fn main() {
         if extension == "pdf" {
             extract_pdf_text(&cli.path);
         } else {
-            let mut lt = LepTess::new(None, "eng").unwrap();
-            lt.set_image(&cli.path).unwrap();
-            lt.set_variable(leptess::Variable::PreserveInterwordSpaces, "11")
-                .unwrap();
-            let tsv = lt.get_tsv_text(0).unwrap();
-
-            let mut words = Vec::new();
-
-            for line in tsv.lines().skip(1) {
-                let columns: Vec<&str> = line.split('\t').collect();
-
-                if columns.len() < 12 {
-                    continue;
-                }
-
-                let conf: f32 = columns[10].parse().unwrap_or(0.0);
-
-                if conf < 70.0 {
-                    continue;
-                }
-                let text = columns[11].to_string();
-
-                if text.trim().is_empty() {
-                    continue;
-                }
-
-                words.push(Word {
-                    text,
-                    left: columns[6].parse().unwrap(),
-                    top: columns[7].parse().unwrap(),
-                    conf,
-                });
-            }
-
-            words.sort_by_key(|w| w.top);
-
-            let mut lines: Vec<Vec<&Word>> = Vec::new();
-
-            for word in &words {
-                if let Some(last_line) = lines.last_mut() {
-                    let last_top = last_line[0].top;
-
-                    if (word.top - last_top).abs() < 10 {
-                        last_line.push(word);
-                        continue;
-                    }
-                }
-                lines.push(vec![word]);
-            }
-
-            for line in &mut lines {
-                line.sort_by_key(|w| w.left);
-            }
-
-            for line in lines {
-                let text = line
-                    .iter()
-                    .map(|w| w.text.as_str())
-                    .collect::<Vec<_>>()
-                    .join(" ");
-
-                println!("{}", text);
-            }
+            extract_image_text(path);
         }
     }
 }
